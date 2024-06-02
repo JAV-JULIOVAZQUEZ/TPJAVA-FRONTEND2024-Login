3774591b770bae592d208b304c753dbedee04e47
Julio <126886676+JVAZQUEZ1601@users.noreply.github.com>
2024-06-02 15:42:11 -0300

    Se agrega API y validaciones al Login

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
     const errorMessage = document.getElementById('errorMessage');
     
+    // Expresión regular para validar el formato del correo electrónico
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
     if (email === '' || password === '') {
         errorMessage.textContent = 'Ambos campos son obligatorios.';
         errorMessage.style.display = 'block';
-
-        
-          
-      
-
+    } else if (!emailRegex.test(email)) {
+        errorMessage.textContent = 'Por favor, ingrese un correo electrónico válido.';
+        errorMessage.style.display = 'block';
     } else {
         errorMessage.style.display = 'none';
-        // Aquí puedes agregar la lógica para redirigir a otra página
-        //alert('Inicio de sesión exitoso!');
+        
         alert('INGRESO EXITOSO');
-        // Por ejemplo, redireccionar a otra página
+        
         window.location.href = 'index.html';
     }
 });
-        
+
